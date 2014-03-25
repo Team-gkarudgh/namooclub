2f3947c0dd8cad3cc97b2e2ef621165656757fd8
kosta-06 <kosta-06@kosta-06-PC>
2014-03-25 15:21:28 +0900

    add login

--- NamooClubThreeWeb/src/com/namoo/ns1/web/main/Login.java
+++ NamooClubThreeWeb/src/com/namoo/ns1/web/main/Login.java
@@ -2,12 +2,14 @@ package com.namoo.ns1.web.main;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.namoo.ns1.service.facade.TownerService;
+import com.namoo.ns1.service.factory.NamooClubServiceFactory;
 @WebServlet("/views/login.do")
 public class Login extends HttpServlet {
 
@@ -23,14 +25,21 @@ public class Login extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		
-		if(true){
-			//로그인성공
-			RequestDispatcher dispatcher = req.getRequestDispatcher("./community/list.do");
-			dispatcher.forward(req, resp);	
+		TownerService service = NamooClubServiceFactory.getInstance().getTownerService();
+		
+		String email = req.getParameter("email");
+		String password = req.getParameter("password");
+		
+		service.loginAsTowner(email, password);
+		
+		if(service.loginAsTowner(email, password) == true){
+			req.getSession().setAttribute("email", email);
+			resp.sendRedirect("./community/list.do");
+			System.out.println("빠밤~!! 로그인>_<");
 		}else{
-			//로그인실패
-		RequestDispatcher dispatcher = req.getRequestDispatcher("./error.jsp");
-		dispatcher.forward(req, resp);
+			req.getSession().removeAttribute("email");
+			resp.sendRedirect("./error.jsp");
+			System.out.println("다시 입력하세요");
 		}
 	}
 
