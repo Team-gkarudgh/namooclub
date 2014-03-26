ebd0f9469f6bb5efabcbfc129b0231f6a907132a
kosta-06 <kosta-06@kosta-06-PC>
2014-03-26 12:44:54 +0900

    hgfdgsa

--- NamooClubThreeWeb/src/com/namoo/ns1/web/club/ClubJoinCtrol.java
+++ NamooClubThreeWeb/src/com/namoo/ns1/web/club/ClubJoinCtrol.java
@@ -8,6 +8,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.namoo.ns1.service.facade.CommunityService;
+import com.namoo.ns1.service.factory.NamooClubServiceFactory;
 @WebServlet("/clubjoinctrol.do")
 public class ClubJoinCtrol extends	HttpServlet{
 
@@ -22,6 +25,15 @@ public class ClubJoinCtrol extends	HttpServlet{
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
+		
+		CommunityService service = NamooClubServiceFactory.getInstance().getCommunityService();
+		
+		String communityName = getInitParameter("communityName");
+		String email = (String) req.getSession().getAttribute("email");
+		
+		service.joinAsMember(communityName, email);
+		
+		System.out.println(communityName);
 
 		RequestDispatcher dispatcher = req.getRequestDispatcher(ClubPageConstants.Join_Page);
 		dispatcher.forward(req, resp);
