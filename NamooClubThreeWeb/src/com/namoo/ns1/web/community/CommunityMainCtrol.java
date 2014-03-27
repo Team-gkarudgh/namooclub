2ab9d561a7138e8c9885789b0d055fa202348458
kosta-06 <kosta-06@kosta-06-PC>
2014-03-27 10:37:32 +0900

    ytfd

--- NamooClubThreeWeb/src/com/namoo/ns1/web/community/CommunityMainCtrol.java
+++ NamooClubThreeWeb/src/com/namoo/ns1/web/community/CommunityMainCtrol.java
@@ -9,19 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.namoo.ns1.web.controller.LoginRequired;
-
 @WebServlet("/communitymainctrol.do")
-@LoginRequired
 public class CommunityMainCtrol extends HttpServlet {
 
 	private static final long serialVersionUID = 6547042998001964975L;
-	
-	@Override
-	protected void process(HttpServletRequest req, HttpServletResponse resp)
-	throws ServletException, IOException{
-		System.out.println("로그인 완료");
-	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
